562e481dc713ed119432530aea183c962e6ff6ee
Bharat <bharatchow.chaparala@gmail.com>
2017-09-29 16:19:33 -0500

    committed at 4:20 on sep 29

--- src/corejava1/Bank.java
+++ src/corejava1/Bank.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package corejava1;
 
 abstract class srmanager{
@@ -18,54 +17,7 @@ public class Bank extends srmanager implements clerk {
 	
 	@Override
 	public void duty() {
-		System.out.println("manager will see how bank is functioning throughout months1");
-		
-	}
-
-	@Override
-	public void cashier() {
-		System.out.println("cashier will service the customer transactions in a months1");
-		
-	}
-
-	@Override
-	void role() {
-		System.out.println("srmanager will conduct meetings with all the employees in a month1");
-		
-	}
-
-	
-public static void main(String[] args) {
-	
-	Bank b= new Bank();
-	b.cashier();
-	b.duty();
-	b.role();
-	
-		
-	}
-}
-=======
-package corejava1;
-
-abstract class srmanager{
-	abstract void role();
-}
-
-interface manager{
-	void duty();
-	
-}
-
-interface clerk extends manager{
-	void cashier();
-	
-}
-public class Bank extends srmanager implements clerk {
-	
-	@Override
-	public void duty() {
-		System.out.println("manager will see how bank is functioning through out the year");
+		System.out.println("manager will see how bank is functioning through out years");
 		
 	}
 
@@ -92,4 +44,3 @@ public static void main(String[] args) {
 		
 	}
 }
->>>>>>> branch 'master' of https://github.com/Bharatchaparalagit/Learningjavasample.git
