12d26b7104c7dc9f1fc08fb441ab7d54d88d6e5b
Bharat <bharatchow.chaparala@gmail.com>
2017-09-30 12:48:30 -0500

    did commit on sep30 12:48

--- src/corejava1/Student1.java
+++ src/corejava1/Student1.java
@@ -5,7 +5,7 @@ public class Student1 {
 	int rollno;
 	String name;
 	String city;
-	static String college="IIT";
+	static String college="EEE";
 		
 	//default constructor
 	public Student1(){
@@ -44,7 +44,7 @@ public class Student1 {
 		Student1 s2=new Student1(111,"bharat");
 		s2.print();  //1111, rasagnya,null
 		
-		Student1 s3=new Student1(2222,"Sowjanya","Hyderabad");
+		Student1 s3=new Student1(2222,"Kiran","Bangalore");
 		s3.print();  //2222,sowjanya, hyderabad..
 		
 		
